d4b9b1ffe7869ad3c30128c0488c40a7b946efec
Hugo Carvalho <webnoico1978@gmail.com>
2025-06-05 21:38:41 +0200

    fix jest config

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,7 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  testMatch: ['**/*.test.(ts|tsx)'],
   testEnvironment: 'node',
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/*.test.(ts|tsx)'], // Look for .test.ts or .test.tsx files
+  roots: ['<rootDir>/tests'],
 };
