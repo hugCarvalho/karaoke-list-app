5cf7666bfdbc167bbb57cc7440c339df7ac81046
Hugo Carvalho <webnoico1978@gmail.com>
2025-06-17 23:19:21 +0200

    .

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -9,13 +9,14 @@ module.exports = {
   moduleNameMapper: {
     "^src/(.*)$": "<rootDir>/src/$1",
   },
-  collectCoverageFrom: [
-    // "src/**/*.{js,jsx,ts,tsx}", // This tells Jest to look at all .js, .jsx, .ts, .tsx files inside the 'src' folder and its subfolders.
-    "!src/**/*.d.ts",           // Exclude TypeScript declaration files
-    // You might also want to exclude other files that are not part of your app logic, e.g.:
-    // "!src/index.tsx",          // Often your main entry point just mounts the app
-    // "!src/reportWebVitals.ts", // Utility for web vitals
-    // "!src/setupTests.ts",       // Jest setup file
-  ],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  // collectCoverageFrom: [
+  //   "src/**/*.{js,jsx,ts,tsx}", // This tells Jest to look at all .js, .jsx, .ts, .tsx files inside the 'src' folder and its subfolders.
+  //   "!src/**/*.d.ts",           // Exclude TypeScript declaration files
+  //   //You might also want to exclude other files that are not part of your app logic, e.g.:
+  //   "!src/index.tsx",          // Often your main entry point just mounts the app
+  //   "!src/reportWebVitals.ts", // Utility for web vitals
+  //   "!src/setupTests.ts",       // Jest setup file
+  // ],
    //collectCoverage: true
 };
