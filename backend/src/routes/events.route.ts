9855a5c9b6f5306dfe3c0951dbcec02f31c562ec
Hugo Carvalho <webnoico1978@gmail.com>
2025-07-23 16:24:32 +0200

    .

--- backend/src/routes/events.route.ts
+++ backend/src/routes/events.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addEventsHandler, closeEventsHandler, getEventsListHandler } from "../controllers/events.controller";
+import { addEventsHandler, closeEventsHandler, getEventsListHandler, getLocationsHandler } from "../controllers/events.controller";
 
 const eventsRoutes = Router()
 
-// prefix: /event
+// prefix: /events
 eventsRoutes.get("/events", getEventsListHandler)
 eventsRoutes.post("/add-event", addEventsHandler)
 eventsRoutes.post("/close-event", closeEventsHandler)
+eventsRoutes.get("/locations", getLocationsHandler)
 export default eventsRoutes
