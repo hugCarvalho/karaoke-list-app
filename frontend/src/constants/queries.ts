33f2fd067fb18bf93321b1585059dce0798d40d5
Hugo Carvalho <webnoico1978@gmail.com>
2025-07-23 16:24:39 +0200

    .

--- frontend/src/constants/queries.ts
+++ frontend/src/constants/queries.ts
@@ -2,5 +2,6 @@ export enum QUERIES {
   GET_USER = "getUser",
   GET_ARTISTS_DB = "getArtistsDb",
   GET_EVENTS_LIST = "getEventsList",
+  GET_LOCATIONS_DB = "getLocationsDb",
   SONGS_LIST = "songsList",
 }
